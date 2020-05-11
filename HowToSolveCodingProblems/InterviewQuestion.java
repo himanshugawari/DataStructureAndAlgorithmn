9b5533624b9f193196a763b282ec0ae6287b67a8
Himanshu Gawari <himanshugawarigit@gmail.com>
2020-05-11 13:32:40 +0000

    solution 1

---
+++ HowToSolveCodingProblems/InterviewQuestion.java
@@ -0,0 +1,39 @@
+//Given 2 arrays, create a function that lets a user know (true or false) whether two arrays contain any common items.
+// e.g. arr1=['a','b','c','x'] arr2=['z','y','i'] Answer=False
+// e.g. brr1=['a','b','c','x'] brr2=['z','y','x'] Answer=True
+
+import java.util.HashMap;
+
+public class InterviewQuestion{
+  public static void main(String[] args){
+    char[] arr1=new char[]{'a','b','c','x'};
+    char[] arr2=new char[]{'z','y','i'};
+
+    char[] brr1=new char[]{'a','b','c','x'};
+    char[] brr2=new char[]{'z','y','x'};
+
+    System.out.println(solution1(arr1,arr2)); // O(n^2) or O(a*b)
+    System.out.println(solution1(brr1,brr2)); // O(n^2) or O(a*b)
+
+  }
+
+  public static boolean solution1(char[] array1,char[] array2){
+    for(int i=0;i<array1.length;i++){
+      for(int j=0;j<array2.length;j++){
+        if(array1[i]==array2[j]){
+          System.out.println("Common Item is : "+array1[i]);
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+/*
+public static boolean solution2(char[] array1,char[] array2){
+  HashMap<Character,Boolean> map=new HashMap<>();
+  for(int i=0;i<array1.length;i++){
+    if(map)
+  }
+}
+*/
+}
