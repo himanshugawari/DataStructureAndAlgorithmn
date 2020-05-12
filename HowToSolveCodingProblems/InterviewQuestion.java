32a8b76f24d94b2cc21b6612cba8ecb1131803df
Himanshu Gawari <himanshugawarigit@gmail.com>
2020-05-12 10:04:59 +0000

    added solution 2

--- HowToSolveCodingProblems/InterviewQuestion.java
+++ HowToSolveCodingProblems/InterviewQuestion.java
@@ -2,6 +2,9 @@
 // e.g. arr1=['a','b','c','x'] arr2=['z','y','i'] Answer=False
 // e.g. brr1=['a','b','c','x'] brr2=['z','y','x'] Answer=True
 
+// 2 parameters - arrays - no size limits
+// return true or false
+
 import java.util.HashMap;
 
 public class InterviewQuestion{
@@ -15,25 +18,38 @@ public class InterviewQuestion{
     System.out.println(solution1(arr1,arr2)); // O(n^2) or O(a*b)
     System.out.println(solution1(brr1,brr2)); // O(n^2) or O(a*b)
 
+    System.out.println("---------");
+
+    System.out.println(solution2(arr1,arr2)); // O(n+n) or O(a+b)  Space Complexity --> O(array1)
+    System.out.println(solution2(brr1,brr2)); // O(n+n) or O(a+b)  Space Complexity --> O(array1)
   }
 
   public static boolean solution1(char[] array1,char[] array2){
     for(int i=0;i<array1.length;i++){
       for(int j=0;j<array2.length;j++){
         if(array1[i]==array2[j]){
-          System.out.println("Common Item is : "+array1[i]);
+          //System.out.println("Common Item is : "+array1[i]);
           return true;
         }
       }
     }
     return false;
   }
-/*
+
 public static boolean solution2(char[] array1,char[] array2){
   HashMap<Character,Boolean> map=new HashMap<>();
   for(int i=0;i<array1.length;i++){
-    if(map)
+    if(!map.containsKey(array1[i])){
+      map.put((Character)array1[i],true);
+    }
   }
+  //System.out.println(map.toString());
+  for(int i=0;i<array2.length;i++){
+    if(map.containsKey(array2[i])){
+      return true;
+    }
+  }
+  return false;
 }
-*/
+
 }
