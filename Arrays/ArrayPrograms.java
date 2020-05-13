125e25a3d08ab44800ee72a4b47662410fced524
Himanshu Gawari <himanshugawarigit@gmail.com>
2020-05-13 10:07:07 +0000

    Array Programs

--- Arrays/ArrayPrograms.java
+++ Arrays/ArrayPrograms.java
@@ -2,9 +2,12 @@ import java.util.*;
 
 public class ArrayPrograms{
   public static void main(String[] args){
-    int[] array=new int[]{1, 2, 2, 3, 3, 3, 3, 4, 4, 5};
-    Search(array,6);
-    ReverseAnArray(array);
+    //int[] array=new int[]{1, 2, 2, 3, 3, 3, 3, 4, 4, 5};
+    Search(new int[]{1, 2, 2, 3, 3, 3, 3, 4, 4, 5},6);
+    ReverseAnArray(new int[]{1, 2, 2, 3, 3, 3, 3, 4, 4, 5});
+    RemoveDuplicatesFromAnSortedArray(new int[]{1, 2, 2, 3, 3, 3, 3, 4, 4, 5});
+    LeftRotate(new int[]{1, 2, 2, 3, 3, 3, 3, 4, 4, 5});
+    LeftRotateByN(new int[]{1, 2, 2, 3, 3, 3, 3, 4, 4, 5},3);
   }
 
   public static void Search(int[] arr,int x){
@@ -30,7 +33,48 @@ public class ArrayPrograms{
       l++;
       h--;
     }
-    System.out.println("Reversed Array "+Arrays.toString(arr));
+    System.out.println("Reversed Array "+Arrays.toString(arr)+"\n");
+  }
+
+  public static void RemoveDuplicatesFromAnSortedArray(int[] arr){
+    System.out.println("With Duplicate Array "+Arrays.toString(arr));
+    int temp=1;
+    for(int i=1;i<arr.length;i++){
+      if(arr[temp-1]!= arr[i]){
+        arr[temp]=arr[i];
+        temp++;
+      }
+    }
+    int[] t=new int[temp];
+    for(int i=0;i<temp;i++){
+      t[i]=arr[i];
+    }
+    System.out.println("Without Duplicate Array "+Arrays.toString(t)+"\n");
+  }
+
+  public static void LeftRotate(int[] arr){
+    System.out.println("Original Array "+Arrays.toString(arr));
+    int temp=arr[0];
+    for(int i=1;i<arr.length;i++){
+      arr[i-1]=arr[i];
+    }
+    arr[arr.length-1]=temp;
+    System.out.println("Left Rotated Array "+Arrays.toString(arr)+"\n");
+  }
+
+  public static void LeftRotateByN(int[] arr,int n){
+    System.out.println("Original Array "+Arrays.toString(arr));
+    int[] temp=new int[n];
+    for(int i=0;i<n;i++){
+      temp[i]=arr[i];
+    }
+    for(int i=n;i<arr.length;i++){
+      arr[i-n]=arr[i];
+    }
+    for(int i=0;i<n;i++){
+      arr[arr.length-n+i]=temp[i];
+    }
+    System.out.println("Left Rotated by "+n+" elements in Array "+Arrays.toString(arr)+"\n");
   }
 
 }
