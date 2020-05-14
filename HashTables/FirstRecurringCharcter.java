d2a3f899d95f29e7066b113def7c0c7799cbe579
Himanshu Gawari <himanshugawarigit@gmail.com>
2020-05-14 11:42:15 +0000

    First Recurring Charcter

---
+++ HashTables/FirstRecurringCharcter.java
@@ -0,0 +1,40 @@
+import java.util.HashSet;
+
+//[2,5,1,5,3,5,1,2,4] ans-> 2
+//[2,1,1,2,3,5,1,2,4] ans-> 1
+//[2,3,4,5] ans-> -1
+
+
+public class FirstRecurringCharcter{
+  public static void main(String[] args){
+    int[] arr1=new int[]{2,5,1,2,3,5,1,2,4};
+    int[] arr2=new int[]{2,1,1,2,3,5,1,2,4};
+    int[] arr3=new int[]{2,3,4,5};
+
+    System.out.println(solution1(arr2));
+    System.out.println(solution2(arr2));
+  }
+
+  public static int solution1(int[] array){
+    HashSet<Integer> hs=new HashSet<>();
+    for(Integer i: array){
+      if(!hs.contains(i)){
+        hs.add(i);
+      }else{
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  public static int solution2(int[] array){
+    for(int i=0;i<array.length;i++){
+      for(int j=i+1;j<array.length;j++){
+        if(array[i]==array[j]){
+          return array[i];
+        }
+      }
+    }
+    return -1;
+  }
+}
